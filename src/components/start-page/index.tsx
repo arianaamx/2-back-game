4aaee8081608b8f366a6ca2b87c83840a6d0141f
Ariana <ariana.milasincic@gmail.com>
2021-04-24 00:36:33 +0100

    Add Start Page component

---
+++ src/components/start-page/index.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import * as colors from "../../utils/theme";
+
+import { useHistory } from "react-router-dom";
+
+const StartPage = () => {
+  const history = useHistory();
+
+  const handlePlayClick = () => {
+    history.push("/game");
+  };
+
+  return (
+    <div>
+      <h1>Hi, today you will be playing a little game called 2-back-game.</h1>
+      <Description>
+        <p>There are going to be 15 letters displayed one after another.</p>
+        <p>
+          Your task is to press <b>SPACE</b> if a letter was shown <b>2 letters</b> before.
+        </p>
+        <div>
+          <button onClick={handlePlayClick}>Let's play it.</button>
+          <button>Can I see an example?</button>
+        </div>
+      </Description>
+    </div>
+  );
+};
+
+export default StartPage;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid ${colors.lightLilac};
+  border-radius: 15px;
+  margin: 10px;
+  padding: 20px;
+
+  p {
+    padding-bottom: 20px;
+    font-size: 18px;
+  }
+`;
