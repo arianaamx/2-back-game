63234e7417a1fb420beeba2bc9fb9a24ec745be9
Ariana <ariana.milasincic@gmail.com>
2021-04-25 00:42:42 +0100

    Add new pastel colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,3 +1,6 @@
 export const lilac = "#BD8FFF";
 export const lightLilac = "#CBA8FF";
 export const lighterLilac = "#F2E8FF";
+
+export const pastelRed = "#FDA1BE";
+export const pastelGreen = "#A1FDB7";
