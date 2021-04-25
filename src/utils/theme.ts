7025ad29b57b4719371a2fd6705d7f65f0a7a47a
Ariana <ariana.milasincic@gmail.com>
2021-04-25 01:09:10 +0100

    Change pastel colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,5 +2,5 @@ export const lilac = "#BD8FFF";
 export const lightLilac = "#CBA8FF";
 export const lighterLilac = "#F2E8FF";
 
-export const pastelRed = "#FDA1BE";
-export const pastelGreen = "#A1FDB7";
+export const pastelRed = "#FF8181";
+export const pastelGreen = "#7CF386";
